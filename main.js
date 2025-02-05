21bd4ca348b1ec17f4012d72fda9736b0d31c8de
BANXPLOID <hatb4033@gmail.com>
2025-02-05 11:58:00 +0700

    Rumah ngadat
    Ngoding

---
+++ main.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body"),
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  searchToggle = document.querySelector(".searchBox");
+
+
+modeToggle.addEventListener("click", () => {
+  modeToogle.classList.toogle(active);
+})
